44e78d9069b973f146e71d245afdd8754daf3a9a
aldosandov <aldosans2016@gmail.com>
2021-01-29 16:59:37 -0600

    routes dir added

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+
 // Settings
 app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+
+// Routes
+app.use(require('./routes/index'))
+
 
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-    res.send({"Title": "Hello World"});
-})
 
 // Starting the server. 
 app.listen(app.get('port'), () => {
