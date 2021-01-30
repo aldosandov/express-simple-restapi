22f9831d1176ed30766a15d88bd699eda1f3bd0f
aldosandov <aldosans2016@gmail.com>
2021-01-29 18:17:47 -0600

    movies route added with get/post methods

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
 const app = express();
 
 
 // Settings
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
+app.use(bodyParser.json())
 
 
 // Routes
-app.use(require('./routes/index'))
+app.use('/api/movies', require('./routes/movies'));
 
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 
 // Starting the server. 
 app.listen(app.get('port'), () => {
     console.log(`Server on port: ${app.get('port')}`);
-})
+});
