ccd77ac25c9a16298c9d0716c000378d9d5d9af4
aldosandov <aldosans2016@gmail.com>
2021-01-29 22:02:31 -0600

    movies route delete method added

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+const _ = require('underscore');
 const router = Router();
 
+
 const movies = require('../sample.json');
+
 
 router.get('/', (req, res) => {
     const data = movies;
     res.json(data)
 });
+
 
 router.post('/', (req, res) => {
     const { title, director, year, rating } = req.body;
@@ -19,5 +23,18 @@ router.post('/', (req, res) => {
         res.status(500).json({error: 'There was an error'});
     }
 });
+
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    _.each(movies, (movie, idx) => {
+        if (movie.id == id) {
+            movies.splice(idx, 1)
+            res.send('deleted')            
+        } else if (id > movies.length)
+            res.status(500).json({error: 'ID not found'})
+    });
+})
+
 
 module.exports = router;
